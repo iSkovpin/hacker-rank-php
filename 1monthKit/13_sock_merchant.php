e73bd83cc971952a8b3fb2fd5af300d2372d9688
Ivan Skovpin <ivanskovpin@gmail.com>
2022-01-11 21:22:59 +0700

    + sockMerchant function

---
+++ 1monthKit/13_sock_merchant.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant($n, $ar) {
+    $pairs = 0;
+    sort($ar);
+
+    for ($idx = 0; $idx < $n - 1; $idx++) {
+        if ($ar[$idx] === $ar[$idx + 1]) {
+            $pairs++;
+            $idx++;
+        }
+    }
+
+    return $pairs;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$ar_temp = rtrim(fgets(STDIN));
+
+$ar = array_map('intval', preg_split('/ /', $ar_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = sockMerchant($n, $ar);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
