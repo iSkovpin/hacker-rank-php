0c7a815f0d0626ea65c701f86c40db17fce32fc9
Ivan Skovpin <ivanskovpin@gmail.com>
2022-01-12 21:50:50 +0700

    + towerBreakers function

---
+++ 1monthKit/16_tower_brteakers.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * Complete the 'towerBreakers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ */
+
+function towerBreakers($n, $m) {
+    if ($m === 1) {
+        return 2;
+    }
+
+    return 2 - $n % 2;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$t = intval(trim(fgets(STDIN)));
+
+for ($t_itr = 0; $t_itr < $t; $t_itr++) {
+    $first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
+
+    $n = intval($first_multiple_input[0]);
+
+    $m = intval($first_multiple_input[1]);
+
+    $result = towerBreakers($n, $m);
+
+    fwrite($fptr, $result . "\n");
+}
+
+fclose($fptr);
